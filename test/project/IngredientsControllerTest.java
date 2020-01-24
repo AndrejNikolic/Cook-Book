f357288bb52f0043cc19c6483cdce66e945f9ef1
AndrejNikolic4217 <existence.of.utopia@gmail.com>
2020-01-24 18:28:22 +0100

    style moved

---
+++ test/project/IngredientsControllerTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Andrej
+ */
+public class IngredientsControllerTest {
+    
+    public IngredientsControllerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test of loadData method, of class IngredientsController.
+     */
+    @Test
+    public void testLoadData() throws Exception {
+    }
+
+    /**
+     * Test of initialize method, of class IngredientsController.
+     */
+    @Test
+    public void testInitialize() {
+    }
+    
+}
